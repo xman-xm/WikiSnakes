72f8995eb0f5a2e38f58db1a551b3219c801e193
xman-xm <151945164+xman-xm@users.noreply.github.com>
2025-01-05 18:54:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
+const express=require('express')
+  
+const app=express()
+app.get('/',(req,res)=>{
     res.send('Hello World!')
 })
-
-
-app.listen('3030',()=>{
-console.log('http://localhost:3030')
-})
+app.listen(process.env.PORT||8080)
